b61941e30dc11273c3c17543e63666b9e05088dd
Varun <varunmayilvaganan11@gmail.com>
2023-08-11 21:05:30 +0530

    updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:todo_firebase/screens/homepage.dart';
+import 'package:todo_firebase/auth/authscreen.dart';
+//import 'package:todo_firebase/screens/homepage.dart';
+
 void main(){
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:const Home(),
-        debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
+      home: AuthScreen(),
+      theme:
+          ThemeData(brightness: Brightness.light, primaryColor: Colors.purple),
+
     );
   }
 }
