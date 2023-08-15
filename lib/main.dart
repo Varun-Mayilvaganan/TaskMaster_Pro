516a22e775550d662b962cb81a47609f22e90d36
Varun <varunmayilvaganan11@gmail.com>
2023-08-15 13:54:47 +0530

    updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_firebase/auth/authscreen.dart';
+import 'package:todo_firebase/screens/homepage.dart';
 //import 'package:todo_firebase/screens/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(){
+
+void main() async {
+  // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -12,9 +21,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthScreen(),
+      home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, usersnapshot)
+      {
+        if (usersnapshot.hasData)
+        {
+          return Home();
+        }
+        else{
+          return AuthScreen();
+        }
+      },
+      ),
       theme:
-          ThemeData(brightness: Brightness.light, primaryColor: Colors.purple),
+          ThemeData(brightness: Brightness.light, primarySwatch: Colors.purple),
 
     );
   }
