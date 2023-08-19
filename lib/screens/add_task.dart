2a961443ce0ebe70b96ac1a338b6ca4df60337c7
Varun <varunmayilvaganan11@gmail.com>
2023-08-19 20:41:57 +0530

    updated all pages

---
+++ lib/screens/add_task.dart
@@ -0,0 +1,100 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class AddTask extends StatefulWidget {
+  const AddTask({super.key});
+
+  @override
+  State<AddTask> createState() => _AddTaskState();
+}
+
+class _AddTaskState extends State<AddTask> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  addtasktofirebase() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    
+    if (user != null) {
+      String uid = user.uid;
+      var time = DateTime.now();
+      await FirebaseFirestore.instance
+          .collection('tasks')
+          .doc(uid)
+          .collection('mytasks')
+          .doc(time.toString())
+          .set({
+        'title': titleController.text,
+        'description': descriptionController.text,
+        'time': time.toString(),
+        'timestamp':time
+      });
+      Fluttertoast.showToast(msg: 'Data Added');
+    } else {
+      Fluttertoast.showToast(msg: 'User not authenticated');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        title: Text('New Task'),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(children: [
+            Container(child: 
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: "Enter Title",
+                labelStyle: GoogleFonts.roboto(fontSize: 16,color: const Color.fromARGB(255, 42, 35, 60)),
+                border: OutlineInputBorder(borderRadius:BorderRadius.circular(8),
+                borderSide: BorderSide(color: Colors.amber)),
+              ),
+            ),),
+            SizedBox(height: 20),
+            Container(child: 
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                labelText: "Enter Description",
+                labelStyle: GoogleFonts.roboto(fontSize: 16,color: const Color.fromARGB(255, 42, 35, 60)),
+                border: OutlineInputBorder(borderRadius:BorderRadius.circular(8)),
+              ),
+            ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState>states){
+                    if (states.contains(MaterialState.pressed))
+                    return Colors.deepPurpleAccent.shade100;
+                  return Colors.deepPurpleAccent;
+                  }
+                  ),
+                  ),
+              onPressed: () {
+                addtasktofirebase();
+              }, 
+                child: Text('Add Task',style: GoogleFonts.roboto(fontSize: 18),),
+                ),
+                ),
+          ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:todo_firebase/screens/add_task.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -7,14 +12,102 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String uid = '';
+
+  @override
+  void initState() {
+    getUid();
+    super.initState();
+  }
+
+  getUid() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+    if (user != null) {
+      setState(() {
+        uid = user.uid;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurpleAccent,
-        
-        
-        title: Text("Do-It"),
+        title: Text("Today Goals"),
+        actions: [
+          IconButton(onPressed: ()async{
+            await FirebaseAuth.instance.signOut();
+
+          }, icon: Icon(Icons.logout))
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.grey.shade400,
+        child: StreamBuilder(
+  stream: FirebaseFirestore.instance
+      .collection('tasks')
+      .doc(uid)
+      .collection('mytasks')
+      .snapshots(),
+  builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return Center(child: CircularProgressIndicator());
+    } else if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
+      return Center(child: Text('No tasks available.'));
+    } else {
+      final docs = snapshot.data!.docs;
+      return ListView.builder(
+        itemCount: docs.length,
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            height: 90,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 20),
+                      child: Text(docs[index]['title'],style: GoogleFonts.roboto(fontSize: 18),)),
+                  ],
+                ),
+                Container(
+                  child: IconButton(
+                    icon: Icon(Icons.delete,
+                    color: Colors.redAccent),
+                    onPressed: () async{
+                      await FirebaseFirestore.instance.collection('tasks').doc(uid).collection('mytasks').doc(docs[index]['time']).delete();
+            
+                    },
+                  ),),
+              ],
+            ),
+          );
+        },
+      );
+    }
+  },
+),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddTask()));
+        },
+        backgroundColor: Color.fromARGB(255, 102, 47, 255),
+        child: Icon(Icons.add),
       ),
     );
   }
